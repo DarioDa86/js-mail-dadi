215686ea41a63c44f8953faaf8f50002a1fea404
DarioDa86 <dario.dagnelli@gmail.com>
2021-07-30 17:41:28 +0200

    fixato nome file index, inserito bottone da fixare

--- js/scriptdadi.js
+++ js/scriptdadi.js
@@ -1,17 +1,25 @@
 //Mail check to enter
+function accessMail() {
 
-//1- chiedere all'utente la sua mail
+// //1- chiedere all'utente la sua mail
 var richiestaMail = prompt("Inserisci la tua e-mail per accedere");
 
 
-//2- verificare se è nella lista e quindi può accedere
-var listaMail = ["ciccio@ciccio.com", "pippo@pippo.com", "birra@birra.com", "tossisco@forte.com" "lancio@dadi.com" ];
+// //2- verificare se è nella lista e quindi può accedere
+var listaMail = ["ciccio@ciccio.com", "pippo@pippo.com", "birra@birra.com", "tossisco@forte.com", "lancio@dadi.com" ];
+console.log(listaMail);
 
-if (richiestaMail.includes(listaMail)) {
-    prompt("Utente riconosciuto, puoi accedere");
-} else {
-    prompt("Non fai parte della banda Bro, sparisci");
-}
+for () {
+    if (richiestaMail.includes(listaMail)) 
 
-//for (var i = )
-//3 stampare messaggio a video se può accedere o no 
+}   else {
+    var accessoNegato = alert("Non fai parte della banda Bro, sparisci");
+
+ {
+//     var autorizzato = alert("Utente riconosciuto, puoi accedere");
+// } else {
+//     var accessoNegato = alert("Non fai parte della banda Bro, sparisci");
+// }
+
+// //for (var i = )
+// //3 stampare messaggio a video se può accedere o no 
