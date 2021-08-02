000dd5de544330d4da4f7c916d020c1ccd699f06
DarioDa86 <dario.dagnelli@gmail.com>
2021-08-02 16:10:50 +0200

    fixato codice js

--- js/scriptdadi.js
+++ js/scriptdadi.js
@@ -1,17 +1,24 @@
+
+var numeroUtente
+var numeroCpu
+
+LanciaDadi();
 // 1- generare numeri casuali da 1 a 6 sia per utente che per CPU
 function LanciaDadi() {
-    var numeroUtente =Math.floor(Math.random() * 6);
+    var numeroUtente = Math.floor(Math.random() * 6);
     console.log("Il tuo numero " + numeroUtente);
+    document.getElementById('punteggio-utente').innerHTML = numeroUtente;
 
-    var numeroCpu =Math.floor(Math.random() * 6);
+    var numeroCpu = Math.floor(Math.random() * 6);
     console.log("Il numero della CPU " + numeroCpu);
+    document.getElementById('punteggio-cpu').innerHTML = numeroCpu;
 
-// 2- stabilire che vince (ovvero punteggio più alto)
+// 2- stabilire che vince (ovvero punteggio più alto) e stampare il risultato
 
     if (numeroUtente > numeroCpu) {
-        alert("hai vinto maledetto fortunello!!");
+        document.getElementById('esito').innerHTML = "Hai vinto maledetto fortunello!!";
     } else {
-        alert("hai perso torna a rubare le autoradio!!");
+        document.getElementById('esito').innerHTML = "Hai perso torna a rubare le autoradio!!";
     }
 
 }
